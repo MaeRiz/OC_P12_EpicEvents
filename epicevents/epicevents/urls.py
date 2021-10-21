c4d1feb5a15c3a71084c0b8b2556ac27d2197c43
Mahery Gonin <mahery.gonin@gmail.com>
2021-10-21 15:36:40 +0200

    Create route

--- epicevents/epicevents/urls.py
+++ epicevents/epicevents/urls.py
@@ -1,21 +1,22 @@
-"""epicevents URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_nested import routers
+
+from app_crm import views
+
+router = routers.SimpleRouter()
+router.register('client', views.ClientViewSet)
+
+client_router = routers.NestedSimpleRouter(router, 'client', lookup='client')
+client_router.register('contract', views.ContractViewSet, basename='client-contract')
+
+contract_router = routers.NestedSimpleRouter(client_router, 'contract', lookup='contract')
+contract_router.register('event', views.EventViewSet, basename='contract-event')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include(router.urls)),
+    path('api/', include(client_router.urls)),
+    path('api/', include(contract_router.urls)),
 ]
