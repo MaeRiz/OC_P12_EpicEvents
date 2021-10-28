ddfa397429e5883e7eac953135e7868d7f4dc450
Mahery Gonin <mahery.gonin@gmail.com>
2021-10-28 10:53:23 +0200

    add blank field and related name

--- epicevents/app_crm/models.py
+++ epicevents/app_crm/models.py
@@ -1,21 +1,23 @@
 from django.db import models
+from django.db.models.query_utils import refs_expression
+from pkg_resources import require
 from app_management.models import CustomUser
 
 
 class Client(models.Model):
-    firstname = models.CharField(max_length=128)
-    lastname = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
-    email = models.CharField(max_length=128)
+    firstname = models.CharField(max_length=128, blank=True)
+    lastname = models.CharField(max_length=128, blank=True)
+    phone = models.CharField(max_length=128, blank=True)
+    email = models.CharField(max_length=128, blank=True)
     prospect = models.BooleanField(default=True)
-    sale_contact = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
-    compagny = models.CharField(max_length=128)
+    sale_contact = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, blank=True, null=True)
+    compagny = models.CharField(max_length=128, blank=True)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
 
 class Contract(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='contract')
     amount = models.IntegerField()
     payement_due = models.DateTimeField()
     stat = models.BooleanField(default=True)
@@ -24,7 +26,7 @@ class Contract(models.Model):
 
 
 class Event(models.Model):
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='event')
     support_contact = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
     date = models.DateTimeField()
     stat = models.BooleanField(default=True)
