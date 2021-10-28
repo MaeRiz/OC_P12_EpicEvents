375957f48bbdfbc6d8f7541dcdc565134b41e2ab
Mahery Gonin <mahery.gonin@gmail.com>
2021-10-28 10:52:51 +0200

    add read only fields

--- epicevents/app_crm/serializer.py
+++ epicevents/app_crm/serializer.py
@@ -8,6 +8,12 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
+        read_only__fields = (
+            'id',
+            'sale_contact',
+            'created_time',
+            'updated_time'
+        )
 
 
 class ContractSerializer(serializers.ModelSerializer):
@@ -15,9 +21,11 @@ class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         fields = '__all__'
+        read_only__fields = ('id', 'created_time', 'updated_time')
 
 class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
         fields = '__all__'
+        read_only__fields = ('id', 'created_time', 'updated_time')
