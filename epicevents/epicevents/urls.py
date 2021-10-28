a401deff384811451588bb33ad9ea083fd91eff2
Mahery Gonin <mahery.gonin@gmail.com>
2021-10-28 10:52:27 +0200

    add prospect and login route

--- epicevents/epicevents/urls.py
+++ epicevents/epicevents/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from app_crm import views
 
 router = routers.SimpleRouter()
 router.register('client', views.ClientViewSet)
+router.register('prospect', views.ProspectViewSet)
 
 client_router = routers.NestedSimpleRouter(router, 'client', lookup='client')
 client_router.register('contract', views.ContractViewSet, basename='client-contract')
@@ -15,6 +17,8 @@ contract_router.register('event', views.EventViewSet, basename='contract-event')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/login/', TokenObtainPairView.as_view()),
 
     path('api/', include(router.urls)),
     path('api/', include(client_router.urls)),
