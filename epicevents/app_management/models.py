16203a660625c159dc85826503cb0f1d57f50c48
Mahery Gonin <mahery.gonin@gmail.com>
2021-11-17 22:06:12 +0100

    Fix hash password on create user

--- epicevents/app_management/models.py
+++ epicevents/app_management/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 ROLE = [
     ('MANAGER', 'MANAGER'),
     ('COMMERCIAL', 'COMMERCIAL'),
@@ -13,8 +12,10 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=128, default="null")
     role = models.CharField(choices=ROLE, max_length=128, default="MANAGER")
 
-    def save(self):
+    def save(self, *args, **kwargs):
         user = super(CustomUser, self)
-        user.set_password(self.password)
+
+        if len(user.password) != 88:
+            user.set_password(self.password)
         user.save()
         return user
